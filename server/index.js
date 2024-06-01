ace9aac241a0c5523d952cb9b43befafad0b73f0
RachelReg <rregulier@hotmail.com>
2024-06-01 15:18:36 +0200

    quest 02 done

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ require("./database/client").checkConnection();
 // Import the Express application from app/config.js
 const app = require("./app/config");
 
+// Déclaration des routes
+
+const sayHello = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayHello);
+
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
